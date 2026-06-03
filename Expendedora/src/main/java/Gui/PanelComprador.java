9cb53f4f678f24609aa277db87d50117430050fc
Bastian <bacabezas2025@udec.cl>
2026-06-02 22:57:44 -0400

    Agregado texto a panel de instrucciones

--- Expendedora/src/main/java/Gui/PanelComprador.java
+++ Expendedora/src/main/java/Gui/PanelComprador.java
@@ -6,16 +6,17 @@ import java.awt.*;
  * Esqueleto de panel cliente parte integrante 2
  */
 public class PanelComprador extends JPanel {
-    JPanel panel1 = new JPanel();
     JPanel panel2 = new JPanel();
     JPanel panel3 = new JPanel();
-    public PanelComprador(ProxyExp Proxy){
+    public PanelComprador(ProxyExp Proxy) {
         this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
-        this.setMinimumSize(new Dimension(500,1000));
-        panel1.setBackground(Color.green); panel1.add(new JLabel("Cliente 1"));
-        panel2.setBackground(Color.cyan); panel2.add(new JLabel("Cliente 2"));
-        panel3.setBackground(Color.blue); panel3.add(new JLabel("Cliente 3"));
-        this.add(panel1);
+        this.setMaximumSize(new Dimension(470, 790));
+        this.setBackground(new Color(190,196,196));
+
+        panel2.setBackground(Color.cyan);
+        panel3.setBackground(Color.blue);
+
+        this.add(new PanelInstrucciones(Proxy));
         this.add(panel2);
         this.add(panel3);
     }
