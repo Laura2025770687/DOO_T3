db682ee8fdcac3278d0fb10066c113ec96692f66
Laura2025770687 <laura.mardones.nueve@gmail.com>
2026-06-09 15:59:32 -0400

    Correcion en las instrucciones del panel de instrucciones

--- Expendedora/src/main/java/Gui/PanelInstrucciones.java
+++ Expendedora/src/main/java/Gui/PanelInstrucciones.java
@@ -6,13 +6,13 @@ import java.awt.*;
 public class PanelInstrucciones extends JPanel {
     public PanelInstrucciones(ProxyExp Proxy) {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.setMaximumSize(new Dimension(470,170));
+        this.setMaximumSize(new Dimension(470,200));
         this.setBackground(new Color(190,196,196));
 
         JLabel titulo = new JLabel("Instrucciones");
-        JLabel instr1 = new JLabel("1. Inserte una moneda a la máquina");
-        JLabel instr2 = new JLabel("2. Seleccione su producto");
-        JLabel instr3 = new JLabel("3. Retire el producto");
+        JLabel instr1 = new JLabel("1. Inserte una moneda a la máquina.");
+        JLabel instr2 = new JLabel("2. Seleccione su producto.");
+        JLabel instr3 = new JLabel("<html><center>3. Retire el producto y su vuelto, si <br> corresponde.</center></html>", SwingConstants.CENTER);
 
         titulo.setAlignmentX(Component.CENTER_ALIGNMENT);
         instr1.setAlignmentX(Component.CENTER_ALIGNMENT);
